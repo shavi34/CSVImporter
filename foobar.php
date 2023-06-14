67b7b6eaf7d743f3e2fad132598a099f2305422b
Shavi Silva <shavinda34@gmail.com>
2023-06-14 16:30:11 +0930

    T2.3 FEAT: When the number is divisible by three (3) and (5) output the word “foobar”

--- foobar.php
+++ foobar.php
@@ -1,6 +1,8 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
-  if ($i % 3 == 0) {
+  if ($i % 3 == 0 && $i % 5 == 0) {
+    echo "foobar\n";
+  } elseif ($i % 3 == 0) {
     echo "foo\n";
   } elseif ($i % 5 == 0) {
     echo "bar\n";
